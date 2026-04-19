a395fc62c9edec437f20eeeee859ae5e09216bac
samykasongo-sk <skikangala@unomaha.edu>
2026-04-19 22:49:54 +0000

    Completed main

--- main.py
+++ main.py
@@ -4,10 +4,12 @@
 # Assignment:
 
 import simpy
+import random
 
 # Global variable to track light state
 greenLight = True
-
+# Queue (cars waiting at red light)
+car_queue = []
 
 def stopLight(env):
     """Simulates a traffic light that cycles between green, yellow, and red."""
@@ -16,6 +18,7 @@ def stopLight(env):
     while True:
         print("Green at time", env.now)
         greenLight = True
+    # let cars pass during green
         yield env.timeout(30)
 
         print("Yellow at time", env.now)
@@ -27,14 +30,27 @@ def stopLight(env):
 
 
 def car(env, car_id):
+
     """Simulates a car arriving and waiting for the light."""
     
-    print("Car", car_id, "arrived at", env.now)
+    print(f"Car", car_id, "arrived at", env.now)
+
+    car_queue.append(car_id)
+
 
     # TODO: Make the car wait while the light is red
+    while (not greenLight) or (len(car_queue) == 0) or (car_queue[0] != car_id):
+        print("car", car_id, "waiting at", env.now)
+        yield env.timeout(1)
     # Hint: use a loop and env.timeout(1)
 
     print("Car", car_id, "departed at", env.now)
+    # remove from queue
+    car_queue.pop(0) 
+
+    # small delay between cars passing
+    yield env.timeout(1)
+
 
 
 def carArrival(env):
@@ -47,8 +63,9 @@ def carArrival(env):
         print("Creating Car", car_id)
 
         # TODO: Start a new car process
+        env.process(car(env, car_id))
 
-        yield env.timeout(5)
+        yield env.timeout(random.randint(3,7))
 
 
 def main():
@@ -58,6 +75,7 @@ def main():
     env.process(stopLight(env))
     
     # TODO: Start the carArrival process
+    env.process(carArrival(env))
 
     # Run simulation
     env.run(until=100)
